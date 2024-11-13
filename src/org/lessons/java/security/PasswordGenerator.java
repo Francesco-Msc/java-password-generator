a4ab3846172bce3831a8927dab972a584dac661a
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-11-13 14:40:18 +0100

    Added user input for name and displayed it

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 public class PasswordGenerator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+        System.out.println("Inserisci il tuo nome e premi invio...");
+        String Name = input.nextLine();
+        System.out.println("Il tuo nome é: " + Name);
+
         input.close();
     }
 }
